812658b198a6a21e961fa01347c4ffd36c5a9543
ranga1729 <rangabandara1729@example.com>
2025-01-02 09:25:08 +0530

    Added sass for stylings

--- api/server.js
+++ api/server.js
@@ -4,11 +4,13 @@ import testRoute from './routes/test.js';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello, Express.js Server!</h1>');
-});
-
 app.use('/test', testRoute);
+
+app.get('/', (req, res) => {
+  res.send({
+    "status": "api running"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
