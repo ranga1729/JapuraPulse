9e10f06bc1c13c04a2ceeacace1378771abf29a8
ranga1729 <rangabandara1729@example.com>
2025-01-03 13:47:49 +0530

    Header modified

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,46 +1,23 @@
-import styles from './Header.module.css'
-import User from '../../assets/user.svg'
+import styles from './Header.module.scss'
 import USJP from '../../assets/USJP.png'
-import Search from '../../assets/search.svg'
-import { useState } from 'react'
 
 function Header() {
-  const[searchTerm, setSearchTerm] = useState<string>('')
+  return <div className={styles.HeaderContainer}>
+    <div className={styles.logoContainer}>
+      <img src={USJP} />
+    </div>
 
-  return( 
-    <>
-      <div className={styles.HeaderContainer}>
+    <div className={styles.nameContainer}>
+      <p className={styles.redLetters}>J'puraPulse</p>
+      
+    </div>
 
-        <div className={styles.logoContainer}>
-          <p className={styles.logo_universityName}>J'Pura</p>
-          <div className={styles.logo_secondTextContainer}>
-            <p>N</p>
-            <img src={USJP} className={styles.USJPLogo}/>
-            <p>W</p>
-          </div>
-        </div>
+    <div className={styles.buttonContainer}>
+      <button className={styles.register}>Register</button>
+      <button className={styles.login}>Login</button>
+    </div>
 
-        <div className={styles.searchContainer}>
-          <div className={styles.searchBar}>
-            <input type='text' placeholder='Search by Event name..' className={styles.searchInput} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-            <button type='submit' className={styles.searchButton}>
-              <img src={Search}className={styles.searchIcon}/>
-            </button>
-          </div>
-        </div>
-
-        <div className={styles.registerButtonContainer}>
-          <p>Register</p>
-        </div>
-
-        <div className={styles.loginButtonContainer}>
-          <img src={User} className={styles.UserImage}></img>
-          <p className={styles.sigingText}>Sign-In</p>
-        </div>
-
-      </div>
-    </>
-  )
+  </div>
 }
 
 export default Header;
--- web/src/layouts/home/Home.tsx
+++ web/src/layouts/home/Home.tsx
@@ -1,32 +1,38 @@
-import styles from './Home.module.css'
+import styles from './Home.module.scss'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer';
-import EventItem from '../../components/Event/EventCard'
+import search from '../../assets/search.svg'
+import EventItem from '../../components/EventCard/EventCard';
 
 function Home() {
-  return (
-    <>
-      <Header />
-      <div className={styles.mainBanner}>
-        <p className={styles.tagline}>What's happening at Japura ?</p>
+  return <>
+    <Header />
+    <div className={styles.banner}>
+      <p>What's New at Japura ?</p>
+
+      <div className={styles.searchContainer}>
+        <input type='text'/>
+        <button>
+          <img src={search}/>
+        </button>
       </div>
 
       <form className={styles.timeFilterContainer}>
-        <input type="radio" id="all" name="timeFilter" value="all" className={styles.timeFilterOption}/>
-        <label htmlFor="all" className={styles.timeFilterLabel}>All</label>
-        <input type="radio" id="thisMonth" name="timeFilter" value="thisMonth" className={styles.timeFilterOption}/>
-        <label htmlFor="thisMonth" className={styles.timeFilterLabel}>This Month</label>
-        <input type="radio" id="nextMonth" name="timeFilter" value="nextMonth" className={styles.timeFilterOption}/>
-        <label htmlFor="nextMonth" className={styles.timeFilterLabel}>Next Month</label>
+        <input type="radio" id="all" name="timeFilter" value="all" />
+        <label htmlFor="all"> All </label>
+        <input type="radio" id="thisMonth" name="timeFilter" value="thisMonth"/>
+        <label htmlFor="thisMonth" > This Month </label>
+        <input type="radio" id="nextMonth" name="timeFilter" value="nextMonth"/>
+        <label htmlFor="nextMonth"> Next Month </label>
       </form>
+    </div>
 
-      <div className={styles.catalog}>
-        <EventItem Title='SHAAN & SANKA LIVE' date='Dec, 24, 2024 : 7.00 am' venue='Hadawila Area' />
-      </div>
+    <div className={styles.catalog}>
+      <EventItem Title='SHAAN & SANKA LIVE' date='Dec, 24, 2024 : 7.00 am' venue='Hadawila Area' />
+    </div>
 
-      <Footer />
-    </>
-  )
+    <Footer />
+  </>
 }
 
 export default Home;
