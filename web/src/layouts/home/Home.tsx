b59ef1785d809543e933f1b02a24c81ac3288299
ranga1729 <rangabandara1729@example.com>
2025-01-07 10:04:13 +0530

    Time filter stylings added

--- web/src/layouts/home/Home.tsx
+++ web/src/layouts/home/Home.tsx
@@ -1,28 +1,35 @@
+import { useEffect, useState } from 'react';
 import styles from './Home.module.scss'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer';
-import search from '../../assets/search.svg'
 import EventItem from '../../components/EventCard/EventCard';
+import { timeFilterOptions } from '../../types/types';
 
 function Home() {
+  const[selectedTimeFilter, setSelectedTimeFilter] = useState<string>(timeFilterOptions.all)
+
+  const handleTimeFilter = (e:string) => {
+    setSelectedTimeFilter(e);
+  }
+
   return <>
     <Header />
     <div className={styles.banner}>
       <p>What's New at Japura ?</p>
 
       <div className={styles.searchContainer}>
-        <input type='text'/>
+        <input type='text' placeholder='Event name...'/>
         <button>
-          <img src={search}/>
+          Search
         </button>
       </div>
 
-      <form className={styles.timeFilterContainer}>
-        <input type="radio" id="all" name="timeFilter" value="all" />
+      <form className={styles.timeFilterContainer} >
+        <input type="radio" id="all" name="timeFilter" value={timeFilterOptions.all} onChange={(e) => handleTimeFilter(e.target.value)} checked={selectedTimeFilter === timeFilterOptions.all} />
         <label htmlFor="all"> All </label>
-        <input type="radio" id="thisMonth" name="timeFilter" value="thisMonth"/>
+        <input type="radio" id="thisMonth" name="timeFilter" value={timeFilterOptions.thisMonth} onChange={(e) => handleTimeFilter(e.target.value)} checked={selectedTimeFilter === timeFilterOptions.thisMonth}/>
         <label htmlFor="thisMonth" > This Month </label>
-        <input type="radio" id="nextMonth" name="timeFilter" value="nextMonth"/>
+        <input type="radio" id="nextMonth" name="timeFilter" value={timeFilterOptions.nextMonth} onChange={(e) => handleTimeFilter(e.target.value)} checked={selectedTimeFilter === timeFilterOptions.nextMonth}/>
         <label htmlFor="nextMonth"> Next Month </label>
       </form>
     </div>
